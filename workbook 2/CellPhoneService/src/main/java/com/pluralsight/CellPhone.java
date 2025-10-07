88c5b7646f48b20ed8ba99ccab399011f190d295
RobertoGG <rguerreroyu@gmail.com>
2025-10-07 13:29:32 -0700

    added overloaded constructor

--- workbook 2/CellPhoneService/src/main/java/com/pluralsight/CellPhone.java
+++ workbook 2/CellPhoneService/src/main/java/com/pluralsight/CellPhone.java
@@ -16,6 +16,13 @@ public class CellPhone {
 
 
     }
+    public CellPhone(int serialNumber, String model, String carrier, String phoneNumber, String owner) {
+        this.SerialNumber = serialNumber;
+        this.model = model;
+        this.carrier = carrier;
+        this.phoneNumber = phoneNumber;
+        this.owner = owner;
+    }
 
 
     public String getcarrier() {
